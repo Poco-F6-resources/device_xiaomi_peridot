ec865ff3449409c3f7b68f92f012aa73f7c93d97
Shikhar <Krtonia@gmail.com>
2025-09-23 21:48:28 +0530

    peridot: Bringup @The-Clover-Project
    Signed-off-by: Shikhar <Krtonia@gmail.com>

--- clover_peridot.mk
+++ clover_peridot.mk
@@ -8,13 +8,21 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Clover-Project stuff.
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
 
 # Inherit from peridot device
 $(call inherit-product, device/xiaomi/peridot/device.mk)
 
-PRODUCT_NAME := lineage_peridot
+# Clover Flags
+CLOVER_BUILDTYPE := UNOFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Device Flags
+PRODUCT_NAME := clover_peridot
 PRODUCT_DEVICE := peridot
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
---
+++ vendorsetup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "Cloning All resources"
+
+# Vendor
+echo "Cloning vendor tree"
+git clone -b lineage-23.0 https://github.com/Poco-F6-resources/proprietary_vendor_xiaomi_peridot.git vendor/xiaomi/peridot
+
+# Kernel sources
+echo "Cloning kernel sources"
+git clone -b lineage-23.0 --depth 1 https://github.com/Poco-F6-resources/android_kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
+
+rm -rf kernel/xiaomi/sm8635-modules
+git clone -b lineage-23.0 --depth 1 https://github.com/Poco-F6-resources/android_kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
+
+rm -rf kernel/xiaomi/sm8635-devicetrees
+git clone -b lineage-23.0 --depth 1 https://github.com/Poco-F6-resources/android_kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
+
+# Hardware xiaomi
+echo "Cloning hardware xiaomi"
+rm -rf hardware/xiaomi
+git clone -b lineage-23.0 https://github.com/Poco-F6-resources/android_hardware_xiaomi.git hardware/xiaomi
+
+# Dolby
+echo "Cloning XiaomiDolby"
+git clone -b lineage-23.0 https://github.com/Poco-F6-resources/android_packages_apps_XiaomiDolby.git packages/apps/XiaomiDolby
